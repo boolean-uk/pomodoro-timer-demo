b99ff533aaa37c7c6f65438b16d49025482cf29c
dearshrewdwit <dearshrewdwit@gmail.com>
2023-02-09 15:11:34 +0000

    rotate arm with progress circle

--- index.js
+++ index.js
@@ -13,26 +13,36 @@
 // When I click the add 1 minute button
 // - add time to the current timer
 
+// Interaction 4
+// As timer decrements
+// - rotate the arm
+// - change the length of the svg circle
+
 // declare variables that hold unchanging state
 const controlBtn = document.querySelector(".control-btn")
 const workTimer = document.querySelector("#workTimer")
 const breakTimer = document.querySelector("#breakTimer")
 const addTimeBtn = document.querySelector("#addTimeBtn")
 const arm = document.querySelector(".arm")
-const workSeconds = 3
-const breakSeconds = 3
+const circle = document.querySelector("#circleTimer")
+const workSeconds = 10
+const breakSeconds = 10
 const increment = 60
+const circumference = 2 * Math.PI * 50
+
 // declare variables that hold changing state
-let isPaused, timerInterval, currentSeconds, currentTimer, isWorkPhase
+let isPaused, timerInterval, currentSeconds, currentTimer, isWorkPhase, currentTotal
 isPaused = true
 isWorkPhase = true
 currentSeconds = workSeconds
+currentTotal = workSeconds
 currentTimer = workTimer
+circle.style.strokeDasharray = circumference
+circle.style.strokeDashoffset = circumference
 
 // Initialise app
 updateTimer(workTimer, workSeconds)
 updateTimer(breakTimer, breakSeconds)
-
 
 controlBtn.addEventListener("click", () => {
   // toggle isPaused value
@@ -45,6 +55,23 @@ controlBtn.addEventListener("click", () => {
   }
 })
 
+addTimeBtn.addEventListener("click", () => {
+  // add to the total time
+  currentSeconds += increment
+  currentTotal += increment
+
+  //update the timer
+  updateTimer(currentTimer, currentSeconds)
+})
+
+function updateTimer(timer, numSeconds) {
+  const minutes = Math.floor(numSeconds / 60) //integer divisions
+  const seconds = numSeconds % 60
+  const formattedSeconds = seconds.toString().padStart(2, '0')
+  // calculate the right time before updating
+  timer.innerText = `${minutes}:${formattedSeconds}`
+}
+
 // calculate new times, update the page, handle ending timer
 function decrement() {
   // don't do anything if paused
@@ -55,6 +82,11 @@ function decrement() {
 
   // format the count and update the page
   updateTimer(currentTimer, currentSeconds)
+
+  // update progress based on new count
+  const percentage = Math.ceil(((currentTotal - currentSeconds) / currentTotal) * 100)
+  // update the progress bar
+  setProgress(percentage)
 
   // if timer ends
   if (currentSeconds === 0) {
@@ -82,19 +114,12 @@ function decrement() {
   }
 }
 
+function setProgress(perc) {
+  // and fills in the circle border
+  const offset = circumference - (perc / 100 * circumference)
+  circle.style.strokeDashoffset = offset
 
-function updateTimer(timer, numSeconds) {
-  const minutes = Math.floor(numSeconds / 60) //integer divisions
-  const seconds = numSeconds % 60
-  const formattedSeconds = seconds.toString().padStart(2, '0')
-  // calculate the right time before updating
-  timer.innerText = `${minutes}:${formattedSeconds}`
+  // sets the arm to rotate
+  const rotation = (perc / 100)* 360
+  arm.style.transform = `rotate(${rotation}deg)`
 }
-
-addTimeBtn.addEventListener("click", () => {
-  // add to the total time
-  currentSeconds += increment
-
-  //update the timer
-  updateTimer(currentTimer, currentSeconds)
-})
